adbb485a7abef0bc12a2246d64ca72f1fb902fca
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-26 20:58:01 -0300

    configurando clerk

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+
+import { ClerkProvider } from '@clerk/nextjs'
+import { ptBR } from '@clerk/localizations'
+
+import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider localization={ptBR}>
+      <html lang="pt-br">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="">
-      <h1>Chaminé&apos;s</h1>
-    </main>
-  )
-}
