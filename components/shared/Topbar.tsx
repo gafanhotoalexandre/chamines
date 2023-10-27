cb61de1e4725d7fa6c8406d587d8b55ec4614934
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-26 21:37:23 -0300

    completando versao 1 do topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
 
 export function Topbar() {
   return (
@@ -10,6 +11,31 @@ export function Topbar() {
           Chaminé&apos;s
         </p>
       </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: 'py-2 px-4',
+            },
+          }}
+        />
+      </div>
     </nav>
   )
 }
