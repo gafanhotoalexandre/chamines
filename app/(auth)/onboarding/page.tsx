aec463f26d3d76a1d91d7f5ae6a2dde67feb2f8f
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-31 17:16:33 -0300

    iniciando criacao do onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,12 @@
 export default async function Page() {
   return (
-    <main>
-      <h1 className="head-text">Onboarding</h1>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Boas-vindas</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete seu perfil agora para usar o Chaminé&apos;s
+      </p>
+
+      <section className="mt-9 p-10 bg-dark-2"></section>
     </main>
   )
 }
