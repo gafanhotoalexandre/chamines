6544b88f750baa00d99113e76127ff2ae8a5c95a
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-11-04 23:31:42 -0300

    criando action thread

---
+++ types/ThreadInfo.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export type ThreadInfo = {
+  _id: string
+  text: string
+  author: mongoose.Types.ObjectId
+  createdAt: Date
+  children: mongoose.Types.ObjectId[]
+  community?: mongoose.Types.ObjectId | undefined
+  parentId?: string | undefined
+}
