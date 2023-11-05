c1b492ab54c9efe9862e458cc8f93ed9b48322b4
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-11-05 00:38:03 -0300

    iniciando desenvolvimento da Home e criacao do ThreadCard component

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,39 @@
+interface ThreadCardProps {
+  id: string
+  currentUserId: string
+  parentId: string | null
+  content: string
+  author: {
+    name: string
+    image: string
+    id: string
+  }
+  community: {
+    id: string
+    name: string
+    image: string
+  } | null
+  createdAt: string
+  comments: {
+    author: {
+      image: string
+    }
+  }
+  isComment?: boolean
+}
+export function ThreadCard({
+  id,
+  currentUserId,
+  parentId,
+  content,
+  author,
+  community,
+  createdAt,
+  comments,
+}: ThreadCardProps) {
+  return (
+    <article>
+      <h2 className="text-small-regular text-light-2">{content}</h2>
+    </article>
+  )
+}
