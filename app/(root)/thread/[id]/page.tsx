b2655a1b2212f0df654f6029f54a3ec398a17f8b
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-11-12 14:54:28 -0300

    listando comentarios

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -47,6 +47,23 @@ export default async function Page({ params }: Params) {
           currentUserId={JSON.stringify(userInfo._id)}
         />
       </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={childItem?.id || ''}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
+      </div>
     </section>
   )
 }
