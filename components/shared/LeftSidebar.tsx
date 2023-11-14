da1db9756cffe52122de1c8e2e71f34a25e56611
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-11-14 20:35:40 -0300

    fix no leftsidebar (menu do perfil)

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,11 +5,14 @@ import Image from 'next/image'
 
 import { sidebarLinks } from '@/constants'
 import { usePathname, useRouter } from 'next/navigation'
-import { SignedIn, SignOutButton } from '@clerk/nextjs'
+import { SignedIn, SignOutButton, useAuth } from '@clerk/nextjs'
 
 export function LeftSidebar() {
   const router = useRouter()
   const pathname = usePathname()
+
+  const { userId } = useAuth()
+
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -17,6 +20,8 @@ export function LeftSidebar() {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route
+
+          if (link.route === '/profile') link.route = `${link.route}/${userId}`
           return (
             <Link
               key={link.label}
